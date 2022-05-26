67567da2e7b96b05c70bc19b6cfe8cd06c1b013f
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-05-26 15:29:58 +0300

    Add grid generation

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let range = document.querySelector('input[type="range"]');
+let grid = document.querySelector('.sketch');
+
+generateGrid();
 
 range.oninput = (e) => {
   let rangeValueElem = document.querySelector('.range-value');
@@ -6,3 +9,29 @@ range.oninput = (e) => {
   rangeValueElem.textContent = e.target.value;
 };
 
+range.onchange = () => generateGrid();
+
+function generateGrid() {
+  let gridSize = document.querySelector('.range-value').textContent;
+
+  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+  let divs = createDivs();
+
+  grid.innerHTML = '';
+  grid.append(divs);
+
+  function createDivs() {
+    let amount = gridSize ** 2;
+    let documentFragment = document.createDocumentFragment();
+
+    for (let i = 0; i < amount; i++) {
+      let div = document.createElement('div');
+
+      documentFragment.append(div);
+    }
+
+    return documentFragment;
+  }
+}
